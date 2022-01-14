86a5e218903f7c0161a8102d1571c6402ffa75e5
youngwjung <youngwjung@gmail.com>
2022-01-15 01:34:43 +0900

    Update ConfigureDjango.sh

--- scripts/ConfigureDjango.sh
+++ scripts/ConfigureDjango.sh
@@ -5,4 +5,4 @@ set -e
 source /opt/venv/bin/activate
 python /opt/django-locallibrary-tutorial/manage.py makemigrations
 python /opt/django-locallibrary-tutorial/manage.py migrate
-python /opt/django-locallibrary-tutorial/manage.py collectstatic
+python /opt/django-locallibrary-tutorial/manage.py collectstatic --no-input
