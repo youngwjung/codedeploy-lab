bccdd538904d08ee62230d71213192005a2adacf
root <root@ip-172-31-47-72.ap-northeast-2.compute.internal>
2022-03-22 03:46:03 +0000

    v1.2

--- scripts/InstallLibrary.sh
+++ scripts/InstallLibrary.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+cd /opt && git clone https://github.com/octo16/django-locallibrary-tutorial.git
+
 source /opt/venv/bin/activate
 sudo pip install -r /opt/django-locallibrary-tutorial/requirements.txt
