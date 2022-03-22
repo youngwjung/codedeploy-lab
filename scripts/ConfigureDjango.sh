946dfac0c3ebbc0ee0dd72ca0f466d022f14db71
octo16 <69204826+octo16@users.noreply.github.com>
2022-03-22 13:13:52 +0900

    Delete scripts directory

--- scripts/ConfigureDjango.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-source /opt/venv/bin/activate
-python /opt/django-locallibrary-tutorial/manage.py makemigrations
-python /opt/django-locallibrary-tutorial/manage.py migrate
-python /opt/django-locallibrary-tutorial/manage.py collectstatic
--- scripts/InstallLibrary.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /opt && git clone https://github.com/octo16/django-locallibrary-tutorial.git
-
-source /opt/venv/bin/activate
-sudo pip install -r requirements.txt
