e8e3a3094283d96d0bc224c20aca6ff364a43630
Young Woo Jung <youngwjung@gmail.com>
2022-04-07 00:59:34 +0000

    fix db init script

--- scripts/InitializeDatabase.sh
+++ scripts/InitializeDatabase.sh
@@ -2,4 +2,9 @@
 
 set -e
 
-psql -h localhost -U postres < /opt/django-locallibrary-tutorial/scripts/init-db.sql
+cat << EOF | psql -h localhost -U postres
+CREATE DATABASElocal_library;
+CREATE USER local_library WITH PASSWORD 'asdf1234';
+GRANT ALL PRIVILEGES ON DATABASE local_library TO local_library;
+ALTER USER local_library CREATEDB;
+EOF
