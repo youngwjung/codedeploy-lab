e44fdfd1857f5912258d5645c778f777ba5d8877
youngwjung <youngwjung@gmail.com>
2022-04-14 17:13:14 +0900

    fix typo

--- scripts/InitializeDatabase.sh
+++ scripts/InitializeDatabase.sh
@@ -3,7 +3,7 @@
 set -e
 
 cat << EOF | psql -h localhost -U postres
-CREATE DATABASElocal_library;
+CREATE DATABASE local_library;
 CREATE USER local_library WITH PASSWORD 'asdf1234';
 GRANT ALL PRIVILEGES ON DATABASE local_library TO local_library;
 ALTER USER local_library CREATEDB;
